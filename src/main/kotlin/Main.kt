b3d7bdf08ad04e5042719a51b31e85e3768dd130
gabmotamelo <gabrielmota6@hotmail.com>
2022-12-29 10:29:08 -0300

    Adicionando conta e saldo

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
 fun main(args: Array<String>) {
     println("Bytebank, bem vindo!")
-    val titular = "Gabriel"
+    val titular:String = "Gabriel"
+    val numeroConta:Int = 1212
+    var saldo = 0.0
+    saldo = 100.0
+    saldo += 300
+
     println("Titular = $titular")
+    println("Numero da conta = $numeroConta")
+    println("Saldo = $saldo")
 }
