c8d1f6de5930bc6f39f9222cc203ac174c30ff65
gabmotamelo <gabrielmota6@hotmail.com>
2022-12-29 12:52:16 -0300

    Adicionando a função depositar

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,8 +5,51 @@ fun main(args: Array<String>) {
     var saldo = 0.0
     saldo = 100.0
     saldo += 300
+    saldo -=300
 
-    println("Titular = $titular")
-    println("Numero da conta = $numeroConta")
-    println("Saldo = $saldo")
+    val contaAlex = Conta()
+    contaAlex.titular = "Gabriel"
+    contaAlex.numero = 23453
+
+    deposito(contaAlex, 40.0)
+
+    println(contaAlex.saldo)
+
+
+
+}
+
+class Conta(){
+    var titular=""
+    var numero = 0
+    var saldo = 0.0
+}
+
+fun deposito(conta: Conta , valor:Double){
+    conta.saldo += valor
+}
+
+fun testaCopiasEReferencias(){
+//    val contaGabriel = Conta()
+//    contaGabriel.titular = "Gabriel"
+//    contaGabriel.numero = 2434
+//    contaGabriel.saldo = 22
+}
+fun testaLacos(saldo:Double){
+    if (saldo > 0.0){
+        println("Conta positiva! ")
+    } else if(saldo == 0.0){
+        println("Conta é neutra!")
+    } else {
+        println("Conta é negativa!")
+    }
+}
+
+fun testaCondicoes(saldo:Double){
+    when {
+        saldo > 0.0 -> println("Saldo positivo!")
+        saldo == 0.0 -> println("Sem saldo!")
+        saldo < 0.0 -> println("Saldo negativo!")
+
+    }
 }
